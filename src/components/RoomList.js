3aac9ccf8c9705d0b8fd63f3eb0cae0a74f4c1fe
laynet <layneingramtaylor@gmail.com>
2018-12-11 16:57:20 -0600

    EOD commit from work computer 12.11.18

--- src/components/RoomList.js
+++ src/components/RoomList.js
@@ -23,10 +23,23 @@ class RoomList extends Component {
 
   render() {
     return(
-      <div>
-        {this.props.rooms.map()}
+      <ul id="room-list">
+        {this.state.rooms.map( room=>
+          <li key={room.key} className
+        )}
+      </ul>
         //need to figure out how to fucking use map again to print the fucking rooms
-      </div>
+
+            {this.state.rooms.map( room =>
+                <li key={room.key} className={ this.props.activeRoom && this.props.activeRoom.key === room.key ? 'active' : '' }>
+                  <button onClick={ () => this.props.setRoom(room) } className="room-name">{ room.name }</button>
+                  { room.creator && this.props.user && room.creator.email === this.props.user.email &&
+                    <button onClick={ () => this.removeRoom(room) } className="remove remove-room-button">&times;</button>
+                  }
+                </li>
+            )}
+</ul>
+
     );
   }
 
