413d09a772c41410ef8135a184867f1f3a0ae8e5
Layne <layneingramtaylor@gmail.com>
2019-01-03 20:34:58 -0600

    EOD changes 1.3

--- src/components/crap.js
+++ src/components/crap.js
@@ -1,16 +1,10 @@
 <ul id="room-list">
-  {this.state.rooms.map( room=>
-    <li key={room.key} className
-  )}
-</ul>
-
-
-      {this.state.rooms.map( room =>
-          <li key={room.key} className={ this.props.activeRoom && this.props.activeRoom.key === room.key ? 'active' : '' }>
-            <button onClick={ () => this.props.setRoom(room) } className="room-name">{ room.name }</button>
-            { room.creator && this.props.user && room.creator.email === this.props.user.email &&
-              <button onClick={ () => this.removeRoom(room) } className="remove remove-room-button">&times;</button>
-            }
-          </li>
-      )}
+            {this.state.rooms.map( room =>
+                <li key={room.key} className={ this.props.activeRoom && this.props.activeRoom.key === room.key ? 'active' : '' }>
+                  <button onClick={ () => this.props.setRoom(room) } className="room-name">{ room.name }</button>
+                  { room.creator && this.props.user && room.creator.email === this.props.user.email &&
+                    <button onClick={ () => this.removeRoom(room) } className="remove remove-room-button">&times;</button>
+                  }
+                </li>
+            )}
 </ul>
