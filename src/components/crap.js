0f4d214d74d609ee420a633505c2c95dbb751328
laynet <layneingramtaylor@gmail.com>
2019-01-23 16:51:30 -0600

    added link to helpful site in crap.js

--- src/components/crap.js
+++ src/components/crap.js
@@ -4,3 +4,6 @@ Verify that the username appears on the screen.
 
 
 https://github.com/MichaelDCooper/chatroom-app/blob/52d25b30f77a901f9487238946322f7fd515a114/src/App.js
+
+
+https://github.com/MichaelDCooper/chatroom-app
