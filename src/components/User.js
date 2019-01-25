1727a1f84e4aef54bb3b9c48658e0b87c58eb89f
laynet <layneingramtaylor@gmail.com>
2019-01-25 12:35:12 -0600

    pop up auth not working

--- src/components/User.js
+++ src/components/User.js
@@ -34,28 +34,10 @@ class User extends Component {
       <div>
         <button onClick={this.signIn}>Sign In</button>
         <button onClick={this.signOut}>Sign Out</button>
-
+        <div>Current User: {this.props.currentUser.displayName}</div>
       </div>
     );
   }
 }
 
 export default User;
-
-// const provider = new this.props.firebase.auth.GoogleAuthProvider();
-// this.props.firebase.auth().signInWithPopup( provider );
-// this.props.firebase.auth().signOut();
-
-//componentDidMount(){
-//registers an onAuthStatechanged event handler
-//this.props.firebase.auth().onAuthStateChanged( user => {
-  //this.props.setUser(user);
-//});
-//}
-
-    //make sign in button
-    //onclick should call firebase's singInWithPopup
-
-    //add signout button that call's firebase signout method
-
-    //Render this.props.user.displayName in the User component so that the current user is visible. Consider rendering the text "Guest" for anonymous users.
